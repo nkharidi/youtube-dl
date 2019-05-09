d5dd1b9194ca056ed68134de18ef0b5fc5d3271c
Malcolm Keefe <Mkeefeus@gmail.com>
2019-05-09 00:42:31 -0400

    Added more to tests

--- youtube_dl/extractor/newsweek.py
+++ youtube_dl/extractor/newsweek.py
@@ -7,13 +7,14 @@ import re
 
 class NewsweekIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?newsweek\.com/'
-
     _TEST = {
         'url': 'https://www.newsweek.com/fortune-100-companies-list-federal-fund-399-billion-1417998',
-        'md5': '3020300c06082a864886f70d020505000410',
+        'md5': 'd15a478c7e7ef165444a2623f420d78b',
         'info_dict': {
             'id': '1417998',
-            'title': 'Fortune 100 Companies Received $399 billion in Federal Funding Over 4 Years'
+            'title': 'Fortune 100 Companies Received $399 billion in Federal Funding Over 4 Years',
+            'description': 'Donald Trump Declares National Emergency To Get Border Wall Funding',
+            'ext': 'mp4'
         }
     }
 
@@ -23,11 +24,12 @@ class NewsweekIE(InfoExtractor):
         website_data = self._download_webpage(url, video_id)
         video_url = re.findall(r'"contentUrl": "(.*?)"', website_data)
         title = re.findall(r'<title>(.*?)</title>', website_data)
-
+        description = re.findall(r'"description": "(.*?)"', website_data)
         return {
             'id': video_id,
             'title': title[0],
-            'url': video_url[0]
+            'url': video_url[0],
+            'description': description[0]
         }
 
 
